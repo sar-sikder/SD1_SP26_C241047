3ae80760b927fd610f96d67424acb47c3444518f
sar-sikder <shahariaabdullahrakib@gmail.com>
2026-03-03 16:55:22 +0600

    initial commit

---
+++ greet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class greet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        input.close();
+    }
+}
---
+++ largest.java
@@ -0,0 +1,28 @@
+import java.util.*; 
+public class largest {
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in); 
+    System.out.print("Enter first number: "); 
+    int num1 = input.nextInt(); 
+
+    System.out.print("Enter second number: "); 
+    int num2 = input.nextInt(); 
+
+    System.out.print("Enter third number: "); 
+    int num3 = input.nextInt(); 
+
+    int max; 
+
+    if(num1 >= num2 && num1 >= num3){
+      max = num1; 
+    }
+    else if(num2 >= num1 && num2 >= num3){
+      max = num2; 
+    }
+    else{
+      max = num3; 
+    }
+    System.out.println("The largest number is: " + max); 
+    input.close(); 
+  }
+}
